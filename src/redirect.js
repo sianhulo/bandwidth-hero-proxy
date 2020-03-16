a65f873af62d1ee3d376311e0ed961d6f9d23df0
sianhulo <sianhulo@users.noreply.github.com>
2020-03-16 17:05:31 -0400

    Adding workaround for nitter
    Any sites with a redirect for a link to serve image should work now, hopefully

--- src/redirect.js
+++ src/redirect.js
@@ -6,7 +6,10 @@ function redirect(req, res) {
   res.removeHeader('expires')
   res.removeHeader('date')
   res.removeHeader('etag')
-  res.setHeader('location', encodeURI(req.params.url))
+  url = decodeURIComponent(req.params.url)             // ensure that the url doesn't get double encoded
+  urlMatch = url.match(/(https?:\/\/)(?!.*\1)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/)[0] // if there's only one url it'll return the same, if there's more than one (determined by the existance of http://) it'll return the last instance
+  if(urlMatch) url = urlMatch // so that if regex returns null it doesn't overwrite the url
+  res.setHeader('location', encodeURI(url))
   res.status(302).end()
 }
 
