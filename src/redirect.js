e5e4333ca222f540247f86718a7745cecc315bbf
simon <si.an.hu.lo@gmail.com>
2020-03-19 11:50:02 -0400

    debug15

--- src/redirect.js
+++ src/redirect.js
@@ -32,21 +32,7 @@ function redirect(req, res, buffer) {
 //   });
   console.log("url status")
   var request = require('request');
-  function handler(req, res) {
-    request('http://www.google.com', function (error, response, body) {
-      console.log(response.statusCode);
-      if (!error && response.statusCode == 200) {
-        console.log("URL is OK") // Print the google web page.
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('URL is OK');
-      } else {
-        res.writeHead(500, {'Content-Type': 'text/html'});
-        res.end('URL broke:'+JSON.stringify(response, null, 2));
-      }
-    })
-  };
-
-  require('http').createServer(handler).listen(4000)
+  console.log(request.get("google.com").status)
 }
 
 module.exports = redirect
