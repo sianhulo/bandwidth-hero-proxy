83e1681532301bd3e53e2b1fbeecc38927070341
simon <si.an.hu.lo@gmail.com>
2020-03-18 22:21:21 -0400

    debug2

--- src/proxy.js
+++ src/proxy.js
@@ -26,6 +26,11 @@ function proxy(req, res) {
     (err, origin, buffer) => {
       if (err || origin.statusCode >= 400) {
         console.log("redirecting from proxy")
+        url = decodeURIComponent(req.params.url)             // ensure that the url doesn't get double encoded
+        urlMatch = url.match(/(https?:\/\/)(?!.*\1)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/)[0] // if there's only one url it'll return the same, if there's more than one (determined by the existance of http://) it'll return the last instance
+        if(urlMatch) req.params.url = urlMatch // so that if regex returns null it doesn't overwrite the url
+        console.log("proxy url")
+        console.log(req.params.url)
         return redirect(req, res)
       }
 
